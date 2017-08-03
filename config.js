0a4c97c35a5d9753887b3f8820521b7e0b00ac5a
Dominic Petrick <dompetrick@gmail.com>
2017-08-03 13:46:36 +0200

    Use env vars for prod image.

---
+++ config.js
@@ -0,0 +1,12 @@
+{
+    backends: [ "aws-cloudwatch-statsd-backend" ],
+    cloudwatch: 
+    {
+        configureFromEnv: true,
+        accessKeyIdEnvKey: 'AWS_ACCESS_KEY_ID',
+        secretAccessKeyEnvKey: 'AWS_SECRET_ACCESS_KEY',
+        regionEnvKey: 'AWS_REGION',
+        verbose: true
+    },
+    port: 8125
+}
